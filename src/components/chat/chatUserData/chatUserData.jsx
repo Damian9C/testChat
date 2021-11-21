9628b3f43c22bbf5670fc27a32bfddb02e2497b5
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-11-21 13:32:08 -0600

    .

--- src/components/chat/chatUserData/chatUserData.jsx
+++ src/components/chat/chatUserData/chatUserData.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import './chatUserData.css';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import InfoChat from "../util/infoChat";
 
 export default function ChatUserData(props) {
+    const [isActive, setIsActive] = React.useState(true);
+    const styleOn = {
+        background: 'red',
+    }
+    const styleOff = {
+        background: 'blue',
+    }
+
     return(
-        <div>
-            <div>
-                <img src={props.userChat.img}/>
-                <p>{props.userChat.name}</p>
-                <p>{props.userChat.phone}</p>
+        <div className="chatUserData">
+            <div className="chatUserData__header">
+                <div>
+                    <img src={props.userChat.img}/>
+                </div>
+                <div>{props.userChat.name}</div>
+                <div className="chatUserData__header--lightTxt">{props.userChat.phone}</div>
             </div>
+            <div className="chatUserData__body ">
+                <button className={isActive? styleOn : styleOff} onClick={() => {
+                    setIsActive(!isActive)
+                }}>
+                    <div>
+                        Datos del Contacto
+                    </div>
+                    <ExpandMoreIcon/>
+                </button>
+            </div>
+            <InfoChat isVisible={isActive} dataUser={props.userChat}/>
         </div>
     )
 }
---
+++ src/components/chat/util/infoChat.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function InfoChat(props) {
+    if (props.isVisible){
+        return(
+            <>
+                <div>
+                    Hola
+                </div>
+            </>
+        )
+    }else {
+        return (
+            <>
+            </>
+        )
+    }
+}
